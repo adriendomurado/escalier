9bb65053dd082d51b1ac420204015b4d65ed79d0
adrien.domurado <adrien.domurado@gmail.com>
2016-02-18 11:58:45 +0100

    Add Dao classes

---
+++ app/src/main/java/fr/domurado/escalier/dao/GameDao.java
@@ -0,0 +1,7 @@
+package fr.domurado.escalier.dao;
+
+/**
+ * Class mirroring the database table Game
+ */
+public class GameDao {
+}
---
+++ app/src/main/java/fr/domurado/escalier/dao/RoundDao.java
@@ -0,0 +1,7 @@
+package fr.domurado.escalier.dao;
+
+/**
+ * Class mirroring the database table RoundDao
+ */
+public class RoundDao {
+}
