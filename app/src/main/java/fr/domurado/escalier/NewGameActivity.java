23ba75182c4b6154b5cbcbbcf5b984a66afd9b9c
adrien.domurado <adrien.domurado@gmail.com>
2016-02-24 16:19:55 +0100

    Add Round Activity

--- app/src/main/java/fr/domurado/escalier/NewGameActivity.java
+++ app/src/main/java/fr/domurado/escalier/NewGameActivity.java
@@ -1,25 +1,21 @@
 package fr.domurado.escalier;
 
-import android.database.sqlite.SQLiteDatabase;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.EditText;
-
-import java.util.concurrent.ExecutionException;
 
 import fr.domurado.escalier.dao.GameDao;
 import fr.domurado.escalier.model.Game;
-import fr.domurado.escalier.task.StartDatabaseTask;
 
 public class NewGameActivity extends AppCompatActivity {
 
+    public static final String EXTRA_GAME_ID = "fr.domurado.escalier.EXTRA_GAME_ID";
     private static String TAG = "NewGameActivity";
 
     @Override
@@ -46,18 +42,36 @@ public class NewGameActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_save) {
+
             final GameDao gameDao = new GameDao(NewGameActivity.this);
             EditText player1 = (EditText) findViewById(R.id.editText_player_1);
             EditText player2 = (EditText) findViewById(R.id.editText_player_2);
             EditText player3 = (EditText) findViewById(R.id.editText_player_3);
+            String player1_string = player1.getText().toString().trim();
+            String player2_string = player2.getText().toString().trim();
+            String player3_string = player3.getText().toString().trim();
 
-            final Game game = new Game(player1.getText().toString(), player2.getText().toString(), player3.getText().toString());
+            if (player1_string.isEmpty()) {
+                player1.setError(getString(R.string.error_name_empty));
+                return true;
+            }
+            if (player2_string.isEmpty()) {
+                player2.setError(getString(R.string.error_name_empty));
+                return true;
+            }
+            if (player3_string.isEmpty()) {
+                player3.setError(getString(R.string.error_name_empty));
+                return true;
+            }
+
+            final Game game = new Game(player1_string, player2_string, player3_string);
             long newGameId = gameDao.save(game);
-            Snackbar.make(findViewById(R.id.newgame_toolbar), "New game saved. Id = " + newGameId, Snackbar.LENGTH_LONG).show();
-            // TODO : start the next activity
-            return true;
+            Log.d(TAG, "New game saved. Id = " + newGameId);
+            Intent intent = new Intent(this, RoundActivity.class);
+            intent.putExtra(EXTRA_GAME_ID, newGameId);
+            startActivity(intent);
+            return false;
         }
 
         return super.onOptionsItemSelected(item);
---
+++ app/src/main/java/fr/domurado/escalier/RoundActivity.java
@@ -0,0 +1,30 @@
+package fr.domurado.escalier;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class RoundActivity extends AppCompatActivity {
+
+    private long gameId;
+    private int currentRound = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_round);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setTitle(getResources().getString(R.string.title_activity_round, currentRound));
+
+        Intent intent = getIntent();
+        gameId = intent.getLongExtra(NewGameActivity.EXTRA_GAME_ID, 0L);
+    }
+}
